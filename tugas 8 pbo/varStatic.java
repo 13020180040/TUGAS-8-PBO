ca579e3242a8a6c3090e8dd2940fb8c931a0a5ad
Wahyuni <61979599+13020180040@users.noreply.github.com>
2020-05-05 05:58:13 -0700

    Add files via upload

---
+++ tugas 8 pbo/varStatic.java
@@ -0,0 +1,9 @@
+public class varStatic {
+    public static void main(String[] args){
+        String firstName = "Wahyuni";
+        String lastName = "sahib";
+        String FULLname = firstName + lastName;
+        
+        System.out.println(FULLname);
+    }
+}
